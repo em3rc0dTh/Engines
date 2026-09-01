99482942f5145e2c67d9253569acc54a903beb54
em3rc0dTh <eduardo.farid@thradex.com>
2026-09-01 12:43:19 -0500

    feat(mk1-s5): register Services snapshot consumer Workflow

--- mk1/runtime/src/orchestration/temporal/workflows/appointment.workflows.ts
+++ mk1/runtime/src/orchestration/temporal/workflows/appointment.workflows.ts
@@ -1,3 +1,4 @@
 export { registerNewCustomerWorkflow } from './register-new-customer.workflow.js';
 export { registerNewAppointmentWorkflow } from './register-new-appointment.workflow.js';
 export { servicesManagementWorkflow } from './services-management.workflow.js';
+export { servicesSnapshotConsumerWorkflow } from './services-snapshot-consumer.workflow.js';
