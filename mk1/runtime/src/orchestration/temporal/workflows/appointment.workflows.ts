be2383c73c3eaee679acd16e389a9bc4aa3cdba6
em3rc0dTh <eduardo.farid@thradex.com>
2026-09-01 12:27:10 -0500

    feat(mk1-s4): register Services management Workflow

--- mk1/runtime/src/orchestration/temporal/workflows/appointment.workflows.ts
+++ mk1/runtime/src/orchestration/temporal/workflows/appointment.workflows.ts
@@ -1,2 +1,3 @@
 export { registerNewCustomerWorkflow } from './register-new-customer.workflow.js';
 export { registerNewAppointmentWorkflow } from './register-new-appointment.workflow.js';
+export { servicesManagementWorkflow } from './services-management.workflow.js';
