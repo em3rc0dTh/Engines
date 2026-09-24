5caa7a94f8c79556cb4abd18e583ae932bfe6632
em3rc0dTh <eduardo.farid@thradex.com>
2026-09-24 11:23:17 -0500

    feat(agent): export A5 conversational experience

---
+++ mk1/runtime/src/agent/experience/index.ts
@@ -0,0 +1,4 @@
+export * from './types.js';
+export * from './progressive-distillation.js';
+export * from './llama-cpp-experience.provider.js';
+export * from './conversational-appointment-experience.js';
